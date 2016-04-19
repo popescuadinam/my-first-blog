98f88b294467cab542715fb1ab2c55ec702e2362
popescuadinam <popescuadinam@gmail.com>
2016-04-19 23:06:25 +0300

    extensions

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,7 @@ from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
